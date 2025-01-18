21f2d823f8e80470fd1d58862e9c5bf83b0d5e66
AniBukhrashvili <92999532+AniBukhrashvili@users.noreply.github.com>
2025-01-18 21:36:46 +0400

    add: mobile header component

--- src/layout/AppLayout.js
+++ src/layout/AppLayout.js
@@ -1,10 +1,11 @@
+import AppHeaderMobile from "../components/AppHeaderMobile";
 import AppSidebar from "../components/AppSidebar";
-
 import styles from "./AppLayout.module.scss";
 
 export default function AppLayout({ children }) {
   return (
     <div className={styles.AppLayout}>
+      <AppHeaderMobile />
       <AppSidebar />
       <div className={styles.AppLayout__Inner}>{children}</div>
     </div>
